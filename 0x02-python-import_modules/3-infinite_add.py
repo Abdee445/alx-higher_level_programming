aeb17bb507e61b8bdad45854bd537c68589dc1bd
Abdee445 <75803488+Abdee445@users.noreply.github.com>
2022-12-02 00:10:25 +0100

    Add files via upload

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    totalsum = 0
-    for i in range(len(sys.argv) - 1):
-        totalsum += int(sys.argv[i + 1])
-    print("{}".format(totalsum))
+    from sys import argv
+    sum = 0
+    for count in range(1, len(argv)):
+        sum += int(argv[count])
+    print("{}".format(sum))
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    allnames = dir(hidden_4)
-    for name in allnames:
-        if name[:2] != "__":
-            print(name)
+    for name in dir(hidden_4):
+        if name[0] != '_':
+            print("{}".format(name))
